e331a9b04c7c987f3e13b839e1af75541a8b085d
bartek <taras_basket@wp.pl>
2024-01-14 14:10:43 +0100

    slash in pets js file

--- routes/pets.js
+++ routes/pets.js
@@ -6,7 +6,7 @@ const checkAuth = require("../middleware/check-auth");
 const PetController = require("../controllers/pet");
 const router = express.Router();
 
-router.post("", checkAuth, extractFile, PetController.createPet);
+router.post("/", checkAuth, extractFile, PetController.createPet);
 router.get("", PetController.getPets);
 router.get("/:id", PetController.getPet);
 router.delete("/:id", checkAuth, PetController.deletePet);
